378a15afaf611d15bb1eaedca1096ab7859a7599
pjmendoz <pearl.j.mendoza@gmail.com>
2019-10-01 14:25:26 -0400

    changing photo and adding in gender and eyes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const nameDiv = document.querySelector('#name')
-// const homeWorld = document.querySelector('#homeworld')
 const hairDiv = document.querySelector('#hair')
-// const gender = document.querySelector('#gender')
+const eyeDiv = document.querySelector('#eyes')
+const genderDiv = document.querySelector('#gender')
+// const homeWorld = document.querySelector('#homeworld')
 
 
 fetch ('https://swapi.co/api/people/85/')
@@ -9,9 +10,14 @@ fetch ('https://swapi.co/api/people/85/')
     .then (function (data) {
         const name = data.name
         const hair = data.hair_color
-        console.log(hair)
-        nameDiv.innerText = `Hello, my name is ${ name }.`
-        hairDiv.innerText = `My hair color is ${ hair }.`
+        const eyes = data.eye_color
+        const gender = data.gender
+
+        console.log(eyes)
+        nameDiv.innerText = `Name: ${ name }`
+        hairDiv.innerText = `Hair Color: ${ hair }`
+        eyeDiv.innerText = `Eye Color: ${ eyes } eyes`
+        genderDiv.innerText = `Gender: ${ gender }`
     })
     
     
